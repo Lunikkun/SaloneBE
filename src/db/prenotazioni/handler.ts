94dfb3228d39faba441ff3a31b8c7c11f99badf1
Lunikk <antoniosica0710@gmail.com>
2024-03-28 01:11:01 +0100

    prenotazione funzionante

--- src/db/prenotazioni/handler.ts
+++ src/db/prenotazioni/handler.ts
@@ -1,13 +1,73 @@
-import { eq } from 'drizzle-orm';
-import { db } from '../../dbConnection';
-import { InsertPrenotazione, Prenotazione, prenotazioni } from './schema';
+import { desc, eq, gte, lte } from "drizzle-orm";
+import { db } from "../../dbConnection";
+import { InsertPrenotazione, Prenotazione, prenotazioni } from "./schema";
+import { SQLiteTextBuilder } from "drizzle-orm/sqlite-core";
+import { selectService } from "../saloonServices/handler";
 
-export async function createPrenotation(pren : InsertPrenotazione){
-
-    await db.insert(prenotazioni).values(pren).returning();
-
+export async function createPrenotation(pren: InsertPrenotazione) {
+  await db.insert(prenotazioni).values(pren).returning();
 }
 
 export async function deletePrenotation(pren: Prenotazione) {
-    await db.delete(prenotazioni).where(eq(prenotazioni.id, pren.id));
+  await db.delete(prenotazioni).where(eq(prenotazioni.id, pren.id));
+}
+
+export async function selectNextPrenotation(date: Date) {
+  let greater = await db
+    .select()
+    .from(prenotazioni)
+    .where(gte(prenotazioni.data_prenotazione, date))
+    .orderBy(prenotazioni.data_prenotazione);
+  //greater.forEach(g=>{console.log(g.data_prenotazione)});
+  return greater[0];
+}
+
+export async function selectPreviousPrenotation(date: Date) {
+  let lesser = await db
+    .select()
+    .from(prenotazioni)
+    .where(lte(prenotazioni.data_prenotazione, date))
+    .orderBy(desc(prenotazioni.data_prenotazione));
+  return lesser[0];
+}
+
+export async function checkPrenotationOverlap(date: Date, durata: number) {
+  let endDate = new Date(date.getTime() + durata);
+  let precedente = await selectPreviousPrenotation(date);
+  let successivo = await selectNextPrenotation(date);
+  //SE UNO DEI 2 CAMPI E' VUOTO???
+  console.log(precedente + " " + successivo);
+
+  if(successivo === undefined && precedente === undefined){
+    return false;
+  }
+  
+  if (precedente === undefined) {
+    if (endDate >= successivo.data_prenotazione) return true;
+    else return false;
+  }
+  if (successivo === undefined) {
+    let previousService = (await selectService(precedente.service_id))[0];
+    let previousServiceDuration = new Date(
+      precedente.data_prenotazione.getTime() +
+        previousService.durata * 1000 * 60
+    );
+    console.log("DATE: " + date + " " + previousServiceDuration);
+    if (date <= previousServiceDuration) return true;
+    else return false;
+  }
+
+  console.log(precedente.data_prenotazione);
+  console.log(successivo.data_prenotazione);
+
+  let previousService = (await selectService(precedente.service_id))[0];
+  let previousServiceDuration = new Date(
+    precedente.data_prenotazione.getTime() + previousService.durata * 1000 * 60
+  );
+  console.log("DATE(entrambi !undefined): " + date + " " + previousServiceDuration);
+  if (date <= previousServiceDuration) {
+    return true;
+  } else if (endDate >= successivo.data_prenotazione) {
+    return true;
+  } else return false;
 }
--- src/userEndpoints.ts
+++ src/userEndpoints.ts
@@ -1,4 +1,5 @@
-import { User } from './db/users/schema';
+import { Service } from "./db/saloonServices/schema";
+import { User } from "./db/users/schema";
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "Hono";
 import { json } from "drizzle-orm/pg-core";
@@ -10,22 +11,30 @@ import {
   deleteCookie,
 } from "hono/cookie";
 import z from "zod";
-import { createPrenotation } from "./db/prenotazioni/handler";
+import {
+  checkPrenotationOverlap,
+  createPrenotation,
+  selectNextPrenotation,
+  selectPreviousPrenotation,
+} from "./db/prenotazioni/handler";
 import { getUserIDFromToken } from "./db/sessions/handler";
-import { InsertPrenotazione } from "./db/prenotazioni/schema";
+import { InsertPrenotazione, Prenotazione } from "./db/prenotazioni/schema";
+import { selectService } from "./db/saloonServices/handler";
+import { ne } from "drizzle-orm";
+import { serve } from "@hono/node-server";
 
 let user = new Hono();
-let UserID : number
+let UserID: number;
 
 user.use("/*", async (c, next) => {
   let cookie = getCookie(c, "ssid");
-  console.log(cookie); 
+  console.log(cookie);
   if (!cookie) {
     return c.body("UNAUTHORIZED_PAGE", { status: 401 });
   }
   let uid = (await getUserIDFromToken(cookie))!;
   UserID = uid;
-  console.log("va")
+  console.log("va");
   await next();
 });
 
@@ -35,24 +44,32 @@ user.post(
     "json",
     z.object({
       data_prenotazione: z.string().datetime(),
-      id_servizio : z.string()
+      id_servizio: z.string(),
     })
   ),
   async (c) => {
-    let { id_servizio, data_prenotazione } = await c.req.json<{id_servizio : number, data_prenotazione : Date}>();
+    let { id_servizio, data_prenotazione } = await c.req.json<{
+      id_servizio: number;
+      data_prenotazione: Date;
+    }>();
     data_prenotazione = new Date(data_prenotazione);
-    console.log(data_prenotazione+" "+id_servizio+ " "+UserID);
-    if(data_prenotazione.getTime()<Date.now()){
-      return c.body("INVALID DATE", {status:400})
+    if (data_prenotazione.getTime() < Date.now()) {
+      return c.body("INVALID DATE", { status: 400 });
     }
-    let prenotazione : InsertPrenotazione = {
-      data_prenotazione,
-      user_id: UserID,
-      service_id : id_servizio
-    }
-    createPrenotation(prenotazione);
-
-    return c.body("OK");
+    let serviceInfo: Service = (await selectService(id_servizio))[0];
+    let overlap = await checkPrenotationOverlap(data_prenotazione, serviceInfo["durata"]*1000*60);
+    if(overlap)
+      return c.body("Data già prenotata", { status: 500 });
+    else 
+      {
+        let prenotazione : InsertPrenotazione = {
+          data_prenotazione,
+           user_id : UserID,
+          service_id : id_servizio
+        }
+        createPrenotation(prenotazione);
+        return c.body("Prenotazione effettuata", {status:200})
+      }
   }
 );
 
